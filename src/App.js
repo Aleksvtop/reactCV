d9728ac3523071167c356edbac2c3f973c0e6975
Aleksvtop <vazilino@gmail.com>
2023-03-06 16:28:06 +0300

    Changed App, Main & Skills components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+import Skills from "./components/skills/Skills";
 
 function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+        </div>
+    );
 }
 
 export default App;
--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Skills.module.css'
 import styleContainer from '../styles/Container.module.css'
+import Skill from "./Skill/Skill";
 
 const Skills = () => {
     return (
@@ -8,7 +9,10 @@ const Skills = () => {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
-
+                    <Skill title={'HTML'} description={"Lorem ipsum dolor sit amet."}/>
+                    <Skill title={'CSS'} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, minus."}/>
+                    <Skill title={'JS'} description={"Lorem ipsum dolor sit amet, consectetur adipisicing."}/>
+                    <Skill title={'React'} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis deleniti exercitationem voluptatem."}/>
                 </div>
             </div>
         </div>
